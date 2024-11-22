277775e45c58d4cc5682f94463273d51d21fee40
quyn03z <quyenvn03@gmail.com>
2024-11-22 16:49:05 +0700

    update new models

---
+++ OnlineShopping.Models/Models/Product.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShopping.Models.Models
+{
+    [Table("Products")]
+    public class Product
+    {
+        [Key]   
+        public int ProductId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public double Price { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public bool Status { get; set; }
+
+        [Required]
+        public int ProductCount { get; set; }
+
+        [Required]
+        public string Imange { get; set; }
+
+        [Range(1, 5)]
+        public int Rating { get; set; }
+
+        [Required]
+        public string SupplierId { get; set; }
+
+        [ForeignKey(nameof(SupplierId))]
+        public virtual Supplier Supplier { get; set; }
+
+        [Required]
+        public int CategoryId { get; set; }
+
+        [ForeignKey(nameof(CategoryId))]
+        public virtual Category Category { get; set; }
+
+    }
+}
--- OnlineShopping.Models/Models/Role.cs
+++ OnlineShopping.Models/Models/Role.cs
@@ -8,18 +8,19 @@ using System.Threading.Tasks;
 
 namespace OnlineShopping.Models.Models
 {
-    [Table("Suppliers")]
-    public class Supplier
+    [Table("Roles")]
+    public class Role
     {
         [Key]
-        public Guid SupplierId {  get; set; }
+        public int Id { get; set; }
 
         [Required]
         public string Name { get; set; }
 
+        [Required]
         public string Description { get; set; }
 
-        public virtual IEnumerable<Product> Products { get; set; }
+        public virtual IEnumerable<User> Users { get; set; }
 
     }
 }
