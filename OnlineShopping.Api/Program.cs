aa71f1a60d0dd54839892b41afd907a17722c840
quyn03z <quyenvn03@gmail.com>
2024-11-22 23:02:02 +0700

    new update

--- OnlineShopping.Api/Program.cs
+++ OnlineShopping.Api/Program.cs
@@ -1,4 +1,7 @@
 
+using Microsoft.EntityFrameworkCore;
+using OnlineShopping.Data;
+
 namespace OnlineShopping.Api
 {
     public class Program
@@ -8,6 +11,13 @@ namespace OnlineShopping.Api
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+
+
+
+            builder.Services.AddDbContext<OnlineShoppingDbContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
